224ddd3f387197fe42a66fd762ba3433686bcc97
JWL16038 <16038@wc.school.nz>
2020-07-08 17:55:32 +1200

    First build for the main menu with functioning HTML & CSS

--- app.py
+++ app.py
@@ -6,14 +6,31 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_NAME = os.path.join(ROOT_DIR, 'smileDatabase.db')
+
+def create_connection(db_file):
+    """create a connection to the sqlite db"""
+    try:
+        connection = sqlite3.connect(db_file)
+        # initialise_tables(connection)
+        return connection
+    except Error as e:
+        print(e)
+
+    return None
+
+
 
 @app.route('/')
 def render_homepage():
     return render_template('home.html')
 
-@app.route('/menu')
+@app.route('/news')
 def render_newspage():
     return render_template('news.html')
+
+
 
 
 
