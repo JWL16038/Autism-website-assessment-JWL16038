319893398a3fc77252a8c59d4c7a1b12f9cfbeb6
JWL16038 <16038@wc.school.nz>
2020-07-12 12:37:38 +1200

    Basic caregivers page working. Beginning to build the homepage screen.

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask,render_template, request, session, redirect
 import sqlite3
 from sqlite3 import Error
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_NAME = os.path.join(ROOT_DIR, 'smileDatabase.db')
+DB_NAME = os.path.join(ROOT_DIR, 'SQLiteDB.db')
 
 def create_connection(db_file):
-    """create a connection to the sqlite db"""
+    "create a connection to the sqlite db"
     try:
         connection = sqlite3.connect(db_file)
         # initialise_tables(connection)
@@ -26,9 +27,20 @@ def create_connection(db_file):
 def render_homepage():
     return render_template('home.html')
 
-@app.route('/news')
+@app.route('/aboutus')
 def render_newspage():
-    return render_template('news.html')
+    return render_template('aboutus.html')
+
+@app.route('/caregivers')
+def render_caregiverspage():
+    con = create_connection(DB_NAME)
+    query = "SELECT SortID,Name,Image,Age,Location,Experience from caregivers"
+    cur = con.cursor()
+    cur.execute(query)
+    caregivers_list = cur.fetchall()
+    con.close()
+
+    return render_template('caregivers.html',caregivers_list= caregivers_list)
 
 
 
