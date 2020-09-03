e2338b6d6156f889309de6c231132ba6e5f0eb25
16038 <16038@wc.school.nz>
2020-09-03 12:31:03 +1200

    Half way through sprint number 3

--- app.py
+++ app.py
@@ -51,28 +51,65 @@ def render_contactpage():
 @app.route('/caregivers')
 def render_caregiverspage():
     con = create_connection(DB_NAME)
-    query = "SELECT SortID,Name,Image,Age,Location,Experience from caregivers_page"
+    query = "SELECT SortID,Name,Image,Age,City,Description,Experience from caregivers_page"
     cur = con.cursor()
     cur.execute(query)
     caregivers_list = cur.fetchall()
     con.close()
     return render_template('caregivers.html',caregivers_list= caregivers_list, logged_in=is_logged_in(), session = session)
 
-@app.route('/caregivers/add/')
+@app.route('/caregivers/add/', methods=["GET","POST"])
 def render_caregiversaddpage():
+    error = None
 
     if not is_logged_in():
         return redirect('/')
 
-    if request.method == "POST":
-        description = request.form.get('description')
-        experience = request.form.get('experience')
-        city = request.form.get('city')
-        profileImage = request.form.get('profileImg')
-        print(description,experience,city,profileImage)
-        return redirect('/')
+    def word_count(str):
+        wordcount = 0
+        words = str.split()
 
-    return render_template('caregivers_add.html', logged_in=is_logged_in(), session = session)
+        for word in words:
+            if word:
+                wordcount += 1
+        return wordcount
+
+    def sendError(error):
+        return render_template('caregivers_add.html', logged_in=is_logged_in(), error=error, session=session)
+
+    if request.method == 'POST':
+        description = request.form.get('description').strip()
+        experience = request.form.get('experience').strip()
+        city = request.form.get('city')
+        profileImage = request.form.get('ProfileImage')
+        fname = session['firstname']
+        lname = session['lastname']
+        age = session['age']
+        userID = session['userID']
+
+        desc_count = word_count(description)
+        exp_count = word_count(experience)
+
+        if desc_count <= 20:
+            error = "length for description too short"
+        elif exp_count <= 20:
+            error = "length for experience too short"
+
+        con = create_connection(DB_NAME)
+        query = "INSERT INTO caregivers_page(SortID,AccountID,FirstName,LastName,Image,Age,City,Description,Experience) VALUES(NULL,?,?,?,?,?,?,?,?)"
+        cur = con.cursor()
+
+        if error:
+            print("error")
+            con.close()
+            sendError(error)
+        else:
+            cur.execute(query,(userID,fname,lname,profileImage,age,city,description,experience))
+            print("no error")
+            con.commit()
+            con.close()
+            return redirect('/')
+    return render_template('caregivers_add.html', logged_in=is_logged_in(), error=error, session=session)
 
 @app.route('/caregivers/book/<caregiverID>')
 def addCaregiver(caregiverID):
@@ -107,6 +144,9 @@ def render_caregiversignup():
         fname = request.form.get('fname').strip().title()
         lname = request.form.get('lname').strip().title()
         email = request.form.get('email').strip().lower()
+        age = request.form.get('age')
+        countryorigin = request.form.get('country')
+        ethnicity = request.form.get('ethnicity')
         password = request.form.get('password')
         password2 = request.form.get('password2')
 
@@ -116,12 +156,12 @@ def render_caregiversignup():
         hashed_password = bcrypt.generate_password_hash(password)
 
         con = create_connection(DB_NAME)
-        query = "INSERT INTO caregiverAccounts(SortID,Firstname,Lastname,email,password) VALUES(NULL,?,?,?,?)"
+        query = "INSERT INTO caregiverAccounts(SortID,Firstname,Lastname,email,Age,CountryOrigin,Ethnicity,password) VALUES(NULL,?,?,?,?)"
         cur = con.cursor()
 
         try:
             if bcrypt.check_password_hash(hashed_password, password2):
-                cur.execute(query, (fname, lname, email, hashed_password))
+                cur.execute(query, (fname, lname, email,age,countryorigin,ethnicity, hashed_password))
                 print("Passwords match")
             else:
                 error = "Passwords don't match"
@@ -168,7 +208,7 @@ def render_logincaregiver():
 
 
         con = create_connection(DB_NAME)
-        query = "SELECT SortID,Firstname,password FROM caregiverAccounts WHERE email = ?"
+        query = "SELECT SortID,Firstname,Lastname,Age,password FROM caregiverAccounts WHERE email = ?"
         cur = con.cursor()
         cur.execute(query,(email,))
         user_data = cur.fetchall()
@@ -177,7 +217,9 @@ def render_logincaregiver():
         try:
             userID = user_data[0][0]
             firstname = user_data[0][1]
-            hashed_password = user_data[0][2]
+            lastname = user_data[0][2]
+            age = user_data[0][3]
+            hashed_password = user_data[0][4]
             if bcrypt.check_password_hash(hashed_password, password):
                 print("Passwords match")
             else:
@@ -193,6 +235,8 @@ def render_logincaregiver():
             session['email'] = email
             session['userID'] = userID
             session['firstname'] = firstname
+            session['lastname'] = lastname
+            session['age'] = age
             return redirect('/')
 
     return render_template('login_caregiver.html', logged_in=is_logged_in(), error=error, )
